27ca088600fd8014c94f6f4bf22c732896362637
trungjc <trungnt@exoplatform.com>
2015-02-27 16:43:53 +0700

    UI-3686: chat area improvement

--- application/src/main/webapp/js/chat.js
+++ application/src/main/webapp/js/chat.js
@@ -837,7 +837,7 @@ var chatApplication = new ChatApplication();
     function addTeamUserLabel(name, fullname) {
       var $usersList = $('.team-users-list');
       var html = $usersList.html();
-      html += "<span class='label team-user-label' data-name='"+name+"'>"+fullname+"&nbsp;&nbsp;<i class='uiIconClose uiIconLightGray team-user-remove'></i></span>";
+      html += "<span class='uiMention'><a href='javascript:void(0)' class='team-user-label' data-name='"+name+"'>"+fullname+"&nbsp;&nbsp;<i class='uiIconClose uiIconLightGray team-user-remove'></i><a/></span>";
       $usersList.html(html);
       var $teamAddUser = $('#team-add-user');
       $teamAddUser.val("");
@@ -847,7 +847,7 @@ var chatApplication = new ChatApplication();
       $userResults.html("");
 
       $(".team-user-remove").on("click", function() {
-        $(this).parent().remove();
+        $(this).parents('.uiMention').remove();
       });
 
     }
@@ -855,7 +855,7 @@ var chatApplication = new ChatApplication();
     function addTaskUserLabel(name, fullname) {
       var $usersList = $('.task-users-list');
       var html = $usersList.html();
-      html += "<span class='label task-user-label' data-fullname='" + fullname + "' data-name='"+name+"'>"+fullname+"&nbsp;&nbsp;<i class='uiIconClose uiIconLightGray task-user-remove'></i></span>";
+      html += "<span class='uiMention'><a href='javascript:void(0)' class=' task-user-label' data-fullname='" + fullname + "' data-name='"+name+"'>"+fullname+"&nbsp;&nbsp;<i class='uiIconClose uiIconLightGray task-user-remove'></i></a></span>";
       $usersList.html(html);
       var $taskAddUser = $('#task-add-user');
       $taskAddUser.val("");
@@ -865,7 +865,7 @@ var chatApplication = new ChatApplication();
       $userResults.html("");
 
       $(".task-user-remove").on("click", function() {
-        $(this).parent().remove();
+        $(this).parents('.uiMention').remove();
       });
     }
 
@@ -1924,10 +1924,10 @@ ChatApplication.prototype.getRoomHtml = function(room, roomPrevUser) {
 ChatApplication.prototype.loadRoom = function() {
   //console.log("TARGET::"+this.targetUser+" ; ISADMIN::"+this.isAdmin);
   if (this.targetUser!==undefined) {
-    jqchat(".users-online").removeClass("info");
+    jqchat(".users-online").removeClass("accordion-active");
     if (this.isDesktopView()) {
       var $targetUser = jqchat("#users-online-"+this.targetUser.replace(".", "-"));
-      $targetUser.addClass("info");
+      $targetUser.addClass("accordion-active");
       jqchat(".room-total").removeClass("room-total-white");
       $targetUser.find(".room-total").addClass("room-total-white");
     }
@@ -2325,7 +2325,7 @@ ChatApplication.prototype.jQueryForUsersTemplate = function() {
 
   if (this.isDesktopView() && this.targetUser!==undefined) {
     var $targetUser = jqchat("#users-online-"+this.targetUser.replace(".", "-"));
-    $targetUser.addClass("info");
+    $targetUser.addClass("accordion-active");
     jqchat(".room-total").removeClass("room-total-white");
     $targetUser.find(".room-total").addClass("room-total-white");
   }
